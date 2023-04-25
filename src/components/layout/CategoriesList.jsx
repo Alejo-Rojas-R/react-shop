038976e966d43d68c5a5f741f51b4ec9236650fc
Alejo-Rojas-R <alejandrorojas1152@gmail.com>
2023-04-24 20:34:11 -0500

    refactor: home page redesign with bootstrap

--- src/components/layout/CategoriesList.jsx
+++ src/components/layout/CategoriesList.jsx
@@ -2,16 +2,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
+import { Badge, Container, Stack, Spinner } from 'react-bootstrap';
 
 export const CategoriesList = () => {
 
-    const { data } = useFetch('https://dummyjson.com/products/categories');
+    // Get categories to be shown as badges below the search input
+    const { data, loading } = useFetch('https://dummyjson.com/products/categories');
+
+    if (loading) {
+        return (
+            <Container className="d-flex align-items-center justify-content-center mt-5">
+                <Spinner animation="border" />
+            </Container>
+        );
+    }
 
     return (
-        <>
-            {data.map((item, index) => {
-                return <NavLink key={index} to={`search/category/${item}`} className="list-group-item">{item}</NavLink>
-            })}
-        </>
-    )
+        <Container className='w-75 pt-5'>
+            <Stack direction='horizontal' className='d-flex flex-wrap justify-content-center' gap={2}>
+                {data.map((item, index) => (
+                    <Badge key={index} pill bg="light" text="black" className='shadow-sm p-2 bg-white rounded'>
+                        <NavLink to={`search/category/${item}`} className='link-underline link-underline-opacity-0 link-underline-opacity-75-hover'>{item}</NavLink>
+                    </Badge>
+                ))}
+            </Stack>
+        </Container>
+    );
 }
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,19 +1,16 @@
 import React, { useState } from 'react'
 import { CategoriesList } from '../layout/CategoriesList';
 import { SearchInput } from '../layout/SearchInput';
+import { Container, Image } from 'react-bootstrap';
 
 export const HomePage = () => {
 
     return (
-        <>
-            <div className="container-fluid mt-3">
-                <div className="row">
-                    <div className="col-md-12">
-                        <SearchInput />
-                        <CategoriesList />
-                    </div>
-                </div>
-            </div>
-        </>
+        <Container className='d-flex flex-column justify-content-center align-items-center vh-100 position-relative'>
+            <h1 className='text-center mb-5'>MyShop</h1>
+            <Image fluid src='src\assets\shopping-basket-blue-clipart-md.png' className='position-absolute opacity-50 z-n1 w-50' alt='background image' />
+            <SearchInput />
+            <CategoriesList />
+        </Container>
     )
 }
